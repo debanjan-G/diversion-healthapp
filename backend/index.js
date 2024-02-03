7d44f0bde9b23f4b9dc0acd99d35a02d896d8bb0
Sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-02-03 18:03:51 +0530

    added quiz feedback & removed unwanted dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import quizFeedback from "./routers/quizFeedback.js";
+
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/feedback", quizFeedback);
+
+
+app.get("/", (req, res) => {
+    res.json("msg: index.js works");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
