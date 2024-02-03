d142cf5ce21766c6a55d495c0d5e915be8cbec41
Debanjan <debanjanghosal10@gmail.com>
2024-02-03 12:12:02 +0530

    Added skeleton of website

---
+++ Frontend/src/assets/Components/BMI App/Bmi.jsx
@@ -0,0 +1,9 @@
+import styles from "./Bmi.module.css";
+
+const Bmi = () => {
+  return (
+    <div className={styles.container}>Here we will have our BMI Calculator</div>
+  );
+};
+
+export default Bmi;
---
+++ Frontend/src/assets/Components/Quiz App/QuizHome.jsx
@@ -0,0 +1,7 @@
+import styles from "./QuizHome.module.css";
+
+const QuizHome = () => {
+  return <div className={styles.container}>Quiz Home Page</div>;
+};
+
+export default QuizHome;
