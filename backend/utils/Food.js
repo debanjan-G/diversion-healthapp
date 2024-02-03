f137592d0c79542d2aa77cb881adb5bb8e942b6d
Ayan564 <ayanmaity564@gmail.com>
2024-02-03 17:02:48 +0530

    Add backend Function

---
+++ backend/utils/Food.js
@@ -0,0 +1,207 @@
+export const BMI_FEEDBACK = [
+  {
+    category: "Severely Underweight",
+    intro:
+      "Your BMI indicates severe underweight, and it's crucial to prioritize your health. Consult with a healthcare professional for personalized advice. Focus on a well-balanced diet that includes a variety of nutrient-dense foods to support your overall well-being.",
+    outro:
+      "Seeking guidance from a healthcare professional is crucial for addressing severe underweight concerns. Incorporating a mix of proteins, healthy fats, and carbohydrates, along with regular medical check-ups, can contribute to your health journey.",
+    foods: [
+      {
+        id: 1,
+        name: "Quinoa",
+        description: "A protein and fiber-rich grain for energy.",
+        img: "https://www.forksoverknives.com/wp-content/uploads/Quinoa_2_750.jpg",
+      },
+      {
+        id: 2,
+        name: "Walnuts",
+        description: "Nutrient-dense nuts with healthy fats.",
+        img: "https://www.thespruceeats.com/thmb/D6GKXSk2NyDKTSrNRLhJV8Iv9f4=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/walnuts-559352-01-5c0e2c9646e0fb00015c8753.jpg",
+      },
+      {
+        id: 3,
+        name: "Spinach",
+        description: "Leafy green rich in vitamins and minerals.",
+        img: "https://www.thespruceeats.com/thmb/k3K_MVpHxTmHOSw2NKF9fg8oytM=/7128x4749/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-933029252-01-9d1ddfad95c24abdbe0a35f7c3e5f86a.jpg",
+      },
+      {
+        id: 4,
+        name: "Chia Seeds",
+        description: "Tiny seeds packed with omega-3 fatty acids.",
+        img: "https://www.mybakingaddiction.com/wp-content/uploads/2017/11/Chia-Seeds-Recipe-5.jpg",
+      },
+      {
+        id: 5,
+        name: "Cottage Cheese",
+        description: "Protein-rich dairy option for muscle support.",
+        img: "https://www.thespruceeats.com/thmb/IlHQGv_XrP4XYjyRwmBzthxGZU8=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/cottage-cheese-59f779b4af5d3a0010d8caba.jpg",
+      },
+    ],
+  },
+  {
+    category: "Moderately Underweight",
+    intro:
+      "Your BMI suggests moderate underweight. It's important to focus on improving your nutrition and overall well-being. Consider incorporating a variety of nutrient-dense foods and consulting with a healthcare professional for personalized guidance.",
+    outro:
+      "Addressing moderate underweight requires a balanced approach. Consult with a healthcare professional for personalized advice. Focus on nourishing your body with a mix of proteins, healthy fats, and carbohydrates to support your health journey.",
+    foods: [
+      {
+        id: 6,
+        name: "Oatmeal",
+        description: "Whole-grain option for sustained energy.",
+        img: "https://www.thespruceeats.com/thmb/B48jqu5vDDrN38ZodhFmE6Dj79s=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/oatmeal-2500-56a20f6b5f9b58b7d0c63b0e.jpg",
+      },
+      {
+        id: 7,
+        name: "Pumpkin Seeds",
+        description: "Nutrient-rich seeds with magnesium and zinc.",
+        img: "https://www.thespruceeats.com/thmb/zc3BpJ4ADLZapUPZD8C1ny3l7_0=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/pumpkin-seeds-close-up-92311204-58f7f73c3df78ca15968d9b6.jpg",
+      },
+      {
+        id: 8,
+        name: "Cauliflower",
+        description: "Versatile vegetable with vitamins and fiber.",
+        img: "https://www.thespruceeats.com/thmb/Nml-b4ZrOF0yLz8qHWDS1_QJ2jo=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-1149300048-5c853afbc9e77c0001fd1a67.jpg",
+      },
+      {
+        id: 9,
+        name: "Mango",
+        description: "Sweet fruit providing vitamins and antioxidants.",
+        img: "https://www.thespruceeats.com/thmb/YT97c1e3nchc6QAlNrBfHrJ8hfs=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/mango-igp-getty-5bb635de4cedfd00256ab6f1.jpg",
+      },
+      {
+        id: 10,
+        name: "Eggs",
+        description: "Protein-packed option with essential nutrients.",
+        img: "https://www.thespruceeats.com/thmb/xGOD4blywHqKg6SOddB5ZHKF3EY=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/are-eggs-dairy-4684029-02-d2b2ed0e87f041e895e816afb7bb1e1f.jpg",
+      },
+    ],
+  },
+  {
+    category: "Healthy Weight",
+    intro:
+      "Congratulations on maintaining a healthy BMI! To sustain your well-being, continue incorporating a balanced diet and staying active. Here are personalized food tips to support your ongoing health journey.",
+    outro:
+      "Maintaining a healthy weight is a significant achievement. These food recommendations are designed to complement your efforts. Feel free to reach out to us if you have any questions or need additional support on your journey to sustained well-being.",
+    foods: [
+      {
+        id: 11,
+        name: "Quinoa Salad",
+        description:
+          "A refreshing salad with quinoa, veggies, and a light dressing.",
+        img: "https://www.thespruceeats.com/thmb/u7H6Ez2b8s_Yc3Mp7VTc_Wj43HE=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/quinoa-salad-5c870f6346e0fb0001f520cc.jpg",
+      },
+      {
+        id: 12,
+        name: "Lean Turkey",
+        description: "Protein-rich turkey for muscle support.",
+        img: "https://www.thespruceeats.com/thmb/8YOiU4Fcig9a-0RHWe9CsqwZw1g=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-635757337-5a91de8f119fa800368bce2b.jpg",
+      },
+      {
+        id: 13,
+        name: "Kale Smoothie",
+        description: "A nutritious smoothie with kale, fruits, and yogurt.",
+        img: "https://www.thespruceeats.com/thmb/RW4NUDkO1wCAqVKjLXQpQ_57QNU=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/kale-smoothie-recipe-5c0e2cbe4cedfd00018e6a8a.jpg",
+      },
+      {
+        id: 14,
+        name: "Blueberries",
+        description: "Antioxidant-rich berries for added health benefits.",
+        img: "https://www.thespruceeats.com/thmb/VzI8NapVCGV1RbKFHtw__4Bl2Jo=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/blueberries-5c870bbf46e0fb000156cc1e.jpg",
+      },
+      {
+        id: 15,
+        name: "Chickpea Salad",
+        description:
+          "A protein and fiber-packed salad with chickpeas and veggies.",
+        img: "https://www.thespruceeats.com/thmb/uR3JTbQq_6p5_YbV-aa91qAzCIo=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/chickpea-salad-recipe-5c770ed146e0fb0001fc9c0a.jpg",
+      },
+    ],
+  },
+  {
+    category: "Overweight",
+    intro:
+      "Your BMI suggests the possibility of overweight. Prioritizing a healthy lifestyle is key. Here are personalized food tips and recommendations to assist you on your journey towards achieving a healthier weight.",
+    outro:
+      "Remember, progress takes time. Gradually incorporate these food tips into your routine and pair them with regular exercise. Small changes can make a big difference in achieving a healthier weight.",
+    foods: [
+      {
+        id: 16,
+        name: "Grilled Salmon",
+        description: "Rich in omega-3 fatty acids and protein.",
+        img: "https://hips.hearstapps.com/hmg-prod/images/how-to-grill-salmon-recipe1-1655870645.jpg?crop=0.8888888888888888xw:1xh;center,top&resize=1200:*",
+      },
+      {
+        id: 17,
+        name: "Sweet Potato",
+        description:
+          "A nutrient-dense, low-calorie option with vitamins and fiber.",
+        img: "https://cdn.loveandlemons.com/wp-content/uploads/2019/08/baked-sweet-potato-recipe-1-480x270.jpg",
+      },
+      {
+        id: 18,
+        name: "Mixed Bean Salad",
+        description:
+          "A mix of beans, veggies, and a light vinaigrette for fiber and protein.",
+        img: "https://greenbowl2soul.com/wp-content/uploads/2019/06/Mixed-bean-salad.jpg",
+      },
+      {
+        id: 19,
+        name: "Olive Oil Drizzled Asparagus",
+        description:
+          "Asparagus cooked with healthy fats for flavor and nutrients.",
+        img: "https://cdn.shopify.com/s/files/1/2802/6066/files/asparagus_parm_7d1def90-d5f8-4c45-9c3a-b095e1aec873_1024x1024.jpg?v=1621891102",
+      },
+      {
+        id: 20,
+        name: "Whole Wheat Pasta",
+        description:
+          "A balanced pasta dish with whole-grain benefits and antioxidants.",
+        img: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2016/3/31/4/FNM_050116-Whole-Wheat-Pasta-with-Sausage-and-Swiss-Chard-Recipe_s4x3.jpg.rend.hgtvcom.616.462.suffix/1459549453002.jpeg",
+      },
+    ],
+  },
+  {
+    category: "Obese",
+    intro:
+      "Your well-being is crucial, and achieving a healthier weight is a commendable goal. Here are personalized food tips and recommendations to guide you on your journey towards improved health and sustainable lifestyle changes.",
+    outro:
+      "Stay committed to your well-being. Integrate these personalized food tips into your routine and consider consulting a professional for guidance. Consistent effort will contribute to your journey towards a healthier weight and lifestyle.",
+    foods: [
+      {
+        id: 21,
+        name: "Mixed Nuts",
+        description: "A variety of nuts for a mix of nutrients.",
+        img: "https://shawsimpleswaps.com/wp-content/uploads/2021/01/Roasted-Mixed-Nuts-1-2.jpg",
+      },
+      {
+        id: 22,
+        name: "Spinach Salad with Grilled Chicken",
+        description:
+          "Leafy greens, grilled chicken, and a light dressing for a satisfying meal.",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKR4wENWgNfNFFyUmvDt21UMUJQ3AbF43OEulrPuXMyK-Y-VN7f-XUPGd6yJvCZrfVogA&usqp=CAU",
+      },
+      {
+        id: 23,
+        name: "Vegetable Stir-Fry",
+        description:
+          "Assorted vegetables stir-fried with lean protein for a nutrient-packed option.",
+        img: "https://www.allrecipes.com/thmb/MF7yU1MBbRlaT40ogVr-1PgggKc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/222658-frozen-vegetable-stir-fry-4x3-1382-583b53fa0bcd4247920611ad431c14cb.jpg",
+      },
+      {
+        id: 24,
+        name: "Quinoa and Black Bean Bowl",
+        description:
+          "A protein and fiber-rich bowl with quinoa, black beans, and veggies.",
+        img: "https://www.bowlofdelicious.com/wp-content/uploads/2014/09/Quinoa-and-Black-Beans-square.jpg",
+      },
+      {
+        id: 25,
+        name: "Grilled Turkey Burger with Avocado",
+        description:
+          "A lean turkey burger topped with avocado for a satisfying and nutritious option.",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjo1UtTgjiz9pLGAWg4oFXZy9WGWO-ct9Rk6uGyy7dKx_Cbi1JX_JJj6CShHMhGIgwkBE&usqp=CAU",
+      },
+    ],
+  },
+];
